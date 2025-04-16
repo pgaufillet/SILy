7abfb933631f18c1902d31cecac280ae09e65bb6
Pierre Gaufillet <pierre.gaufillet@bergamote.eu>
2025-04-16 13:33:21 +0200

    Introduce basic Arduino OTA capability. LittlFS image building and upload with espota wired as tasks.

--- SILy.ino
+++ SILy.ino
@@ -16,10 +16,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <Preferences.h>
-#include <WiFi.h>
+#include <ArduinoOTA.h>
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
+#include <Preferences.h>
+#include <WiFi.h>
 
 #include "SILyPreferences.h"
 
@@ -31,104 +32,134 @@ AsyncWebServer server(80);
 
 SILyPreferences *silyPrefs;
 
-void setup()
-{
-    pinMode(BOARD_LED, OUTPUT);
-    digitalWrite(BOARD_LED, LED_ON);
+void setup() {
+  pinMode(BOARD_LED, OUTPUT);
+  digitalWrite(BOARD_LED, LED_ON);
 
-    Serial.begin(115200);
-    sleep(1);
-    Serial.println("[INFO] Starting board...");
+  Serial.begin(115200);
+  sleep(1);
+  Serial.println("[INFO] Starting board...");
 
-    Serial.println("[INFO] Starting NVS...");
-    silyPrefs = new SILyPreferences();
+  Serial.println("[INFO] Starting NVS...");
+  silyPrefs = new SILyPreferences();
 
-    Serial.println(silyPrefs->generateJson());
-    Serial.println("[INFO] NVS started");
+  Serial.println(silyPrefs->generateJson());
+  Serial.println("[INFO] NVS started");
 
-    Serial.print("[INFO] Starting LittleFS...");
-    if (!LittleFS.begin(false))
-    {
-        Serial.println(" Unable to mount LittleFS filesystem.");
-        return;
+  Serial.print("[INFO] Starting LittleFS...");
+  if (!LittleFS.begin(false)) {
+    Serial.println(" Unable to mount LittleFS filesystem.");
+    return;
+  } else {
+    Serial.println("ok");
+  }
+
+  Serial.println("[INFO] Setting up WiFi...");
+  String hostname = silyPrefs->get("general", "hostname");
+  String ssid = silyPrefs->get("wifi", "ssid");
+  String password = silyPrefs->get("wifi", "password");
+
+  WiFi.setHostname(hostname.c_str());
+  Serial.print("[INFO] Hostname: ");
+  Serial.println(WiFi.getHostname());
+  if (silyPrefs->get("wifi", "mode") == "Access Point") {
+    WiFi.mode(WIFI_AP);
+    WiFi.softAP(ssid, password);
+    Serial.print("[INFO] Broadcasting Wifi AP ");
+    Serial.println(WiFi.softAPSSID());
+    Serial.print("[INFO] AP IP: ");
+    Serial.println(WiFi.softAPIP());
+  } else {
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.printf("[INFO] Connecting to WiFi %s...\n", ssid.c_str());
+      delay(1000);
     }
-    else
-    {
-        Serial.println("ok");
-    }
 
-    Serial.println("[INFO] Setting up WiFi...");
-    String hostname = silyPrefs->get("general", "hostname");
-    String ssid = silyPrefs->get("wifi", "ssid");
-    String password = silyPrefs->get("wifi", "password");
+    Serial.print("[INFO] IP: ");
+    Serial.println(WiFi.localIP());
+  }
+  Serial.println("[INFO] WiFi started");
 
-    WiFi.setHostname(hostname.c_str());
-    Serial.print("[INFO] Hostname: ");
-    Serial.println(WiFi.getHostname());
-    if (silyPrefs->get("wifi", "mode") == "Access Point")
-    {
-        WiFi.mode(WIFI_AP);
-        WiFi.softAP(ssid, password);
-        Serial.print("[INFO] Broadcasting Wifi AP ");
-        Serial.println(WiFi.softAPSSID());
-        Serial.print("[INFO] AP IP: ");
-        Serial.println(WiFi.softAPIP());
-    }
-    else
-    {
-        WiFi.begin(ssid, password);
-        while (WiFi.status() != WL_CONNECTED)
-        {
-            Serial.printf("[INFO] Connecting to WiFi %s...\n", ssid.c_str());
-            delay(1000);
+  Serial.println("[INFO] Setting up Web server...");
+  server.onNotFound([](AsyncWebServerRequest *request) { request->send(404); });
+  Serial.println("[INFO] Error handler...ok");
+
+  server.on("/config", HTTP_GET, [](AsyncWebServerRequest *request) {
+    String json = silyPrefs->generateJson();
+    Serial.print("[DEBUG] Sending: ");
+    Serial.println(json);
+    request->send(200, "application/json", silyPrefs->generateJson());
+  });
+  Serial.println("[INFO] GET config handler...ok");
+
+  server.on("/config/reset", HTTP_POST, [](AsyncWebServerRequest *request) {
+    Serial.println("[DEBUG] Reset config");
+    silyPrefs->reset();
+    request->send(200);
+  });
+  Serial.println("[INFO] POST config reset handler...ok");
+
+  // Beware here: data is NOT NULL terminated. It can therefore not be cast
+  // directly into char*
+  server.on(
+      "/config", HTTP_POST,
+      [](AsyncWebServerRequest *request) { request->send(200); }, NULL,
+      [](AsyncWebServerRequest *request, uint8_t *data, size_t len,
+         size_t index, size_t total) {
+        String json = String((const char *)data, len);
+        Serial.print("[DEBUG] Receiving: ");
+        Serial.println(json);
+        silyPrefs->parseJson(json);
+      });
+  Serial.println("[INFO] POST config handler...ok");
+
+  server.serveStatic("/", LittleFS, "/").setDefaultFile("/index.html");
+  Serial.println("[INFO] Static files handler...ok");
+
+  server.begin();
+  Serial.println("[INFO] Web server started");
+
+  // Hostname defaults to esp3232-[MAC]
+  ArduinoOTA.setHostname(hostname.c_str());
+  ArduinoOTA
+      .onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH) {
+          type = "sketch";
+        } else { // U_SPIFFS
+          type = "filesystem";
+          LittleFS.end();
         }
 
-        Serial.print("[INFO] IP: ");
-        Serial.println(WiFi.localIP());
-    }
-    Serial.println("[INFO] WiFi started");
+        Serial.println("Start updating " + type);
+      })
+      .onEnd([]() { Serial.println("\nEnd updating"); })
+      .onProgress(
+          [](unsigned int progress, unsigned int total) { Serial.printf("."); })
+      .onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) {
+          Serial.println("Auth Failed");
+        } else if (error == OTA_BEGIN_ERROR) {
+          Serial.println("Begin Failed");
+        } else if (error == OTA_CONNECT_ERROR) {
+          Serial.println("Connect Failed");
+        } else if (error == OTA_RECEIVE_ERROR) {
+          Serial.println("Receive Failed");
+        } else if (error == OTA_END_ERROR) {
+          Serial.println("End Failed");
+        }
+      });
 
-    Serial.println("[INFO] Setting up Web server...");
-    server.onNotFound([](AsyncWebServerRequest *request)
-                      { request->send(404); });
-    Serial.println("[INFO] Error handler...ok");
+  ArduinoOTA.begin();
+  Serial.println("[INFO] OTA started");
 
-    server.on("/config", HTTP_GET, [](AsyncWebServerRequest *request)
-              { 
-                String json = silyPrefs->generateJson();
-                Serial.print("[DEBUG] Sending: ");
-                Serial.println(json);
-                request->send(200, "application/json", silyPrefs->generateJson()); });
-    Serial.println("[INFO] GET config handler...ok");
-
-    server.on("/config/reset", HTTP_POST, [](AsyncWebServerRequest *request)
-              { 
-                Serial.println("[DEBUG] Reset config");
-                silyPrefs->reset(); 
-                request->send(200); });
-    Serial.println("[INFO] POST config reset handler...ok");
-
-    // Beware here: data is NOT NULL terminated. It can therefore not be cast directly into char*
-    server.on("/config", HTTP_POST, [](AsyncWebServerRequest *request)
-              { request->send(200); }, NULL, [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
-              { 
-                String json = String((const char *) data, len);
-                Serial.print("[DEBUG] Receiving: ");
-                Serial.println(json);
-                silyPrefs->parseJson(json); });
-    Serial.println("[INFO] POST config handler...ok");
-
-    server.serveStatic("/", LittleFS, "/").setDefaultFile("/index.html");
-    Serial.println("[INFO] Static files handler...ok");
-
-    server.begin();
-    Serial.println("[INFO] Web server started");
-
-    Serial.println("[INFO] Board started");
+  Serial.println("[INFO] Board started");
 }
 
-void loop()
-{
-    sleep(5);
-    // Serial.println("[DEBUG] I'm alive!");
+void loop() {
+  delay(150);
+  ArduinoOTA.handle();
+  // Serial.println("[DEBUG] I'm alive!");
 }
